ecb57da04d2ede21113fa4f1b14901ad46c355cd
jaky-sheikh <jaky@eu4m.eu>
2025-02-07 14:52:30 +0600

    registered with authentication

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,30 @@
 import Lottie from "lottie-react";
 import registerLottieData from "../../assets/Lottie/register.json";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext/AuthContext";
 
 const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        // password validation and validation error
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,14 +37,14 @@ const Register = () => {
 
                     <div className="card-body">
                         <h1 className="text-5xl font-bold">Register now!</h1>
-                        <fieldset className="fieldset">
+                        <form onSubmit={handleRegister} className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" name="email" className="input" placeholder="Email" />
                             <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" name="password" className="input" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Register</button>
-                        </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
