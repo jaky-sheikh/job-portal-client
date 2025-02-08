2007389260941cec9c5a79d644087e943e67577f
jaky-sheikh <jaky@eu4m.eu>
2025-02-08 19:19:38 +0600

    private route and redirect

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext/AuthContext";
 import SocialLogin from "../Shared/SocialLogin";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 
 const SignIn = () => {
 
     const { signInUser } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log('in sign in page', location)
+    const from = location.state || '/';
 
     const handleSignIn = e => {
         e.preventDefault();
@@ -18,6 +23,7 @@ const SignIn = () => {
         signInUser(email, password)
             .then(result => {
                 console.log('sign in', result.user)
+                navigate(from);
             })
             .catch(error => {
                 console.log(error);
